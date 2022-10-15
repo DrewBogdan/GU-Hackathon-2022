43304cf5605e9c591f5bfb17161fed1ccde56afe
Vinny D <thevinnycast@gmail.com>
2022-10-15 14:24:15 -0700

    sound test implemented

--- Hackathon_Game/Assets/Scripts/ApproachSound.cs
+++ Hackathon_Game/Assets/Scripts/ApproachSound.cs
@@ -7,12 +7,13 @@ public class ApproachSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //
     }
 }
